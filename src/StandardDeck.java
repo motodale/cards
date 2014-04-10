2444c3812b06a2bd5e9f682393c59ba32b298c0f
Dale Tibbetts <dale.tibbetts@banno.com>
2014-04-09 20:04:52 -0500

    forgot the deck

---
+++ src/StandardDeck.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Dale.
+ */
+public class StandardDeck implements IDeckOfCards {
+    ArrayList<IPlayingCard> myCards;
+
+    public StandardDeck(){
+        myCards = new ArrayList<IPlayingCard>();
+
+        for(int j = 0; j<5; j++){
+            for(int i = 1; i <= 12; i++){
+                myCards.add(new StandardPlayingCard());
+            }
+        }
+        for(int i = 0; i < 52; i++){
+            myCards.add(new StandardPlayingCard());
+        }
+    }
+
+
+    @Override
+    public IPlayingCard drawCard(){
+        IPlayingCard topCard = myCards.get(0);
+        myCards.remove(0);
+
+        return topCard;
+    }
+
+    @Override
+    public void addCard(IPlayingCard cardToAdd){
+        myCards.add(cardToAdd);
+    }
+
+    @Override
+    public void shuffle(){
+        java.util.Collections.shuffle(myCards);
+    }
+
+    @Override
+    public void sort(){
+        java.util.Collections.sort(myCards);
+    }
+
+    @Override
+    public String toString(){
+        StringBuffer output = new StringBuffer();
+
+        for(IPlayingCard aCard: myCards){
+            output.append(aCard.toString() + " ");
+        }
+
+        return output.toString();
+    }
+
+    @Override
+    public boolean isEmpty(){
+
+    }
+
+
+}
