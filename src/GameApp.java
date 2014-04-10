483a74661ad34ffc580b7a9a214beb3bee87df22
motodale <dale.tibbetts@banno.com>
2014-04-09 22:05:37 -0500

    uhhh yep

---
+++ src/GameApp.java
@@ -0,0 +1,22 @@
+import playingcards.*;
+
+
+public class GameApp
+{
+
+
+
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        
+        PlayingCardFrame myFrame = new PlayingCardFrame(new StandardDeck());
+        
+        myFrame.setVisible(true);
+        
+    }
+
+}
